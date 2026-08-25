bd32cc266f5357bfdfa073cedd49e702303be263
Abhishek Khade <163422985+abhishekkhade1@users.noreply.github.com>
2026-08-25 18:06:30 +0530

    Time: 1 ms (87.66%), Space: 44.8 MB (99.62%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,35 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> partial_ans = new ArrayList<>();
+
+        boolean[] used = new boolean[nums.length];
+
+        helper(nums, ans, partial_ans, used);
+        return ans;
+
+    }
+
+    public void helper(int[] nums, List<List<Integer>> ans, List<Integer> partial_ans, boolean[] used){
+
+        if(partial_ans.size() == nums.length){
+            ans.add(new ArrayList(partial_ans));
+            return;
+        }
+
+        for(int i=0; i<nums.length; i++ ){
+
+            if(used[i]){
+                continue;
+            }
+
+            partial_ans.add(nums[i]);
+            used[i] = true;
+            helper(nums, ans, partial_ans, used);
+
+            used[i] = false;
+            partial_ans.remove(partial_ans.size()-1);
+        }
+
+    }
+}
