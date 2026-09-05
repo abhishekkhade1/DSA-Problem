5ffca00953b7e0f3db20e1ba84bbfa74c5b0ba6f
Abhishek Khade <163422985+abhishekkhade1@users.noreply.github.com>
2026-09-05 14:23:54 +0530

    Time: 0 ms (100%), Space: 47.6 MB (18.4%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,49 +15,41 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        // this function delete the key node from given tree and return the updated root
 
-        if(root == null) return null;
+        if (root == null)
+            return null;
 
-        if(key < root.val){
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
             root.left = deleteNode(root.left, key);
-        }
-        else if(key > root.val){
-            root.right = deleteNode(root.right , key);
-        }
-        else{
-            // found key node to be delete
-            // case 1:Both child nodes of key node are null
-            if(root.right == null && root.left == null) return null;
+        } else {
 
-            else{
-                // case 3: Both child of key node are non-null
-                if(root.right != null && root.left != null){
-                    int inSuccesser = inorderSuccesser(root);
-                    root.val = inSuccesser;
-                    root.right = deleteNode(root.right , inSuccesser);
-                }
-                else{
-                    // case 2:one child are non-null;
-                    if(root.right!=null){
-                        return root.right;
-                    }
-                    if(root.left!=null){
-                        return root.left;
-                    }
-                }
+            if (root.right == null && root.left == null) {
+                return null;
             }
+
+            if (root.left == null) {
+                return root.right;
+            }
+
+            if (root.right == null) {
+                return root.left;
+            }
+
+            TreeNode succssoer = findSuccssor(root.right);
+            root.val = succssoer.val;
+            root.right = deleteNode(root.right, succssoer.val);
         }
         return root;
     }
 
-    private int inorderSuccesser(TreeNode node) {
-    TreeNode curr = node.right;
+    public TreeNode findSuccssor(TreeNode node) {
 
-    while (curr.left != null) {
-        curr = curr.left;
+        TreeNode curr = node;
+        while (curr.left != null) {
+            curr = curr.left;
+        }
+        return curr;
     }
-
-    return curr.val;
-}
 }
